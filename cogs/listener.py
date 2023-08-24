9bad6459b54912e19c31cfd00ffc98646a9e9ca9
Spuds1224 <zachydem@aol.com>
2023-08-24 01:25:57 -0400

    fix: urls not being checked for watermark

--- cogs/listener.py
+++ cogs/listener.py
@@ -7,14 +7,7 @@ import numpy as np
 from discord.ext import commands
 
 import requests
-from io import BytesIO
-
-import logging
-import logging.config
-
-# sets up logging
-logging.config.fileConfig('logging.conf', defaults={'logfilename': '/home/pi/Bot_Data/ifunny_crusader/log.txt'})
-logger = logging.getLogger("defaultLogger")
+from PIL import Image
 
 class IFunnyDetector(commands.Cog):
     def __init__(self, bot):
@@ -41,69 +34,43 @@ class IFunnyDetector(commands.Cog):
                 return False
             
     # function to handle embedded links as well as real images
-    async def watermark_detector(self, image: Union[discord.Attachment, str]):
-            match type(image):
-                case discord.message.Attachment:
-                    image_bytes = await image.read()
-                case str():
-                    response = requests.get(image)
-                    image_data = BytesIO()
-                    image_data.write(response.content)
-                    image_bytes = image_data.getvalue()
-                case _:
-                    raise Exception("Invalid parameter!")
-            
-            arr = np.fromstring(string=image_bytes, dtype=np.uint8)
-            img = cv2.imdecode(arr, -1) # 'Load it as it is' https://stackoverflow.com/questions/21061814/how-can-i-read-an-image-from-an-internet-url-in-python-cv2-scikit-image-and-mah
+    def watermark_detector(self, image: Union[discord.Attachment, str]):
+        match type(image):
+            case discord.message.Attachment:
+                image_bytes = image.read()
+            case discord.message.Message:
+                response = requests.get(image.content)
+                image_bytes = response.content
+            case _:
+                raise Exception("Invalid parameter!")
 
-            # compares the picture with the ifunny watermark
-            img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # http://www.learningaboutelectronics.com/Articles/How-to-match-an-image-embedded-in-another-image-Python-OpenCV.php
-            template = cv2.imread("ifunny_watermark.jpg", 0)
-            result = cv2.matchTemplate(img_gray, template, cv2.TM_SQDIFF_NORMED)
+        arr = np.fromstring(string=image_bytes, dtype=np.uint8)
+        img = cv2.imdecode(arr, -1) # 'Load it as it is' https://stackoverflow.com/questions/21061814/how-can-i-read-an-image-from-an-internet-url-in-python-cv2-scikit-image-and-mah
 
-            # determines if the ifunny watermark is in the image
-            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-            if min_val < 0.01: # found this code from google ai
-                return True
-            else:
-                return False
+        # compares the picture with the ifunny watermark
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # http://www.learningaboutelectronics.com/Articles/How-to-match-an-image-embedded-in-another-image-Python-OpenCV.php
+        template = cv2.imread("ifunny_watermark.jpg", 0)
+        result = cv2.matchTemplate(img_gray, template, cv2.TM_SQDIFF_NORMED)
+
+        # determines if the ifunny watermark is in the image
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+        if min_val < 0.01: # found this code from google ai
+            return True
+        else:
+            return False
 
     @commands.Cog.listener()
     async def on_message(self, message):
         # gets the url to every attachment in the message
         if message.attachments != []:
-            logger.debug("Attachments found!")
             for attachment in message.attachments:
                 if self.image_checker(attachment): # is it a valid image?
-                    logger.debug("Attachment is valid image!")
                     if self.watermark_detector(attachment): # does it have the watermark?
-                        logger.debug("Attachment has watermark!")
                         await message.reply("**IFUNNY DETECTED**\n**ANTI-CRINGE COUNTERMEASURES DEPLOYED**")
         else:
                 if self.image_checker(message): # is it a valid image?
                     if self.watermark_detector(message): # does it have the watermark?
                         await message.reply("**IFUNNY DETECTED**\n**ANTI-CRINGE COUNTERMEASURES DEPLOYED**")
-
-            # for i in message.attachments:
-            #     try:
-            #         # gets image and makes it grayscale
-            #         image_bytes = await i.read()
-            #         arr = np.fromstring(string=image_bytes, dtype=np.uint8) # type: ignore
-            #         img = cv2.imdecode(arr, -1) # 'Load it as it is' https://stackoverflow.com/questions/21061814/how-can-i-read-an-image-from-an-internet-url-in-python-cv2-scikit-image-and-mah
-
-            #         # compares the picture with the ifunny watermark
-            #         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # http://www.learningaboutelectronics.com/Articles/How-to-match-an-image-embedded-in-another-image-Python-OpenCV.php
-            #         template = cv2.imread("ifunny_watermark.jpg", 0)
-            #         result = cv2.matchTemplate(img_gray, template, cv2.TM_SQDIFF_NORMED)
-
-            #         # determines if the ifunny watermark is in the image
-            #         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-            #         if min_val < 0.01: # found this code from google ai
-            #             await message.reply("**IFUNNY DETECTED**\n**ANTI-CRINGE COUNTERMEASURES DEPLOYED**")
-            #     except cv2.error:
-            #         pass
-            
-    # def watermark_detector(image)
 
 def setup(bot): # this is called by Pycord to setup the cog
     bot.add_cog(IFunnyDetector(bot)) # add the cog to the bot
