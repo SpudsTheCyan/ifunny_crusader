3424bfebe4ec303f34b97124251311e5c5b8642e
Spuds1224 <zachydem@aol.com>
2023-09-01 15:35:56 -0400

    feat: added ability to match links in message

--- cogs/listener.py
+++ cogs/listener.py
@@ -1,5 +1,6 @@
 #listener.py
 import logging
+import re
 from typing import Union
 
 import cv2
@@ -18,38 +19,44 @@ class IFunnyDetector(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    def image_checker(self, file:Union[discord.Attachment, discord.message.Message]) -> bool:
-        """Takes an attachment or a message as an argument and returns a bool based on weather it is an image or not
+    def image_checker(self, file:discord.Attachment | discord.message.Message) -> bool | list[str]:
+        """Takes an attachment as an argument and returns a bool based on weather it is an image or not. Can also take a message as an argument and return a list of image links, or false if there are none.
 
         Args:
             attachment (discord.Attachment | discord.message.Message): Either an attachment or a link.
         """
-        match type(file):
-            case discord.message.Attachment:
+        match file:
+            case discord.message.Attachment():
                 if str(file.content_type)[0:5] =="image":
                     return True
                 else:
                     return False
-            case discord.message.Message:
-                if (file.content[-4:] == ".jpg" or file.content[-4:] == ".png") and file.content[0:8] == "https://": # does it start with https and end with .jpg or .png?
-                    return True
-                else:
+            case discord.message.Message():
+                links = re.findall("(https?://.*.(?:png|jpg))", file.content)
+                if links == []:
                     return False
+                else:
+                    return links
+
+                # if (file.content[-4:] == ".jpg" or file.content[-4:] == ".png") and file.content[0:8] == "https://": # does it start with https and end with .jpg or .png?
+                #     return True
+                # else:
+                #     return False
             case _:
                 return False
             
     # function to handle embedded links as well as real images
     async def watermark_detector(self, image: Union[discord.Attachment, str]):
-        match type(image):
-            case discord.message.Attachment:
+        match image:
+            case discord.message.Attachment():
                 image_bytes = await image.read()
-            case discord.message.Message:
-                response = requests.get(image.content)
+            case str():
+                response = requests.get(image)
                 image_bytes = response.content
             case _:
                 raise Exception("Invalid parameter!")
 
-        arr = np.fromstring(string=image_bytes, dtype=np.uint8)
+        arr = np.frombuffer(image_bytes, dtype=np.uint8)
         img = cv2.imdecode(arr, -1) # 'Load it as it is' https://stackoverflow.com/questions/21061814/how-can-i-read-an-image-from-an-internet-url-in-python-cv2-scikit-image-and-mah
 
         # compares the picture with the ifunny watermark
@@ -76,12 +83,15 @@ class IFunnyDetector(commands.Cog):
                         if watermarkDetected: # does it have the watermark?
                             await message.reply("**IFUNNY DETECTED**\n**ANTI-CRINGE COUNTERMEASURES DEPLOYED**")
                             messageSent = True
-        elif not messageSent:
-            if self.image_checker(message): # is it a valid image?
-                watermarkDetected = await self.watermark_detector(message)
-                if watermarkDetected: # does it have the watermark?
-                    await message.reply("**IFUNNY DETECTED**\n**ANTI-CRINGE COUNTERMEASURES DEPLOYED**")
-                    messageSent = True
+        else:
+            links = self.image_checker(message)
+            if isinstance(links, list): # if links were found in the message
+                for link in links:
+                    if not messageSent:
+                        watermarkDetected = await self.watermark_detector(link)
+                        if watermarkDetected: # does it have the watermark?
+                            await message.reply("**IFUNNY DETECTED**\n**ANTI-CRINGE COUNTERMEASURES DEPLOYED**")
+                            messageSent = True
 
 def setup(bot): # this is called by Pycord to setup the cog
     bot.add_cog(IFunnyDetector(bot)) # add the cog to the bot
